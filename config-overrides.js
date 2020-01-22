1dc167e4139c5ded037b7e5dfc60ed15e67bdeca
Nick McCurdy <nick@nickmccurdy.com>
2020-01-22 18:28:56 -0500

    Move Jest config to package.json

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,9 @@
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
-module.exports = {
-  webpack: config => ({
-    ...config,
-    plugins: [
-      ...config.plugins,
-      new MonacoWebpackPlugin({ languages: ["typescript"] })
-    ]
-  }),
-  jest: config => ({
-    ...config,
-    setupFiles: ["jest-canvas-mock"],
-    transformIgnorePatterns: ["node_modules/(?!monaco-editor/)"]
-  })
-};
+module.exports = config => ({
+  ...config,
+  plugins: [
+    ...config.plugins,
+    new MonacoWebpackPlugin({ languages: ["typescript"] })
+  ]
+});
