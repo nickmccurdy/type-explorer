5e8bc4826d47447ee552cc7e64a193722d5e45f0
Nick McCurdy <nick@nickmccurdy.com>
2020-02-14 07:48:57 -0500

    Test TypeScript as an example package with types

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { fireEvent, render, wait } from "@testing-library/react";
 import App from "./App";
 
-test("renders search results", async () => {
-  const { container, getByLabelText, getByText } = render(<App />);
-  expect(container.querySelector(".monaco-editor")).toBeNull();
+test.each(["react", "typescript"])(
+  'renders search results for "%s"',
+  async value => {
+    const { container, getByLabelText, getByText } = render(<App />);
+    expect(container.querySelector(".monaco-editor")).toBeNull();
 
-  fireEvent.change(getByLabelText("Package:"), { target: { value: "react" } });
-  fireEvent.click(getByText("Submit"));
-  await wait(() => {
-    expect(container.querySelector(".monaco-editor")).not.toBeNull();
-  });
-});
+    fireEvent.change(getByLabelText("Package:"), { target: { value } });
+    fireEvent.click(getByText("Submit"));
+    await wait(() => {
+      expect(container.querySelector(".monaco-editor")).not.toBeNull();
+    });
+  }
+);
